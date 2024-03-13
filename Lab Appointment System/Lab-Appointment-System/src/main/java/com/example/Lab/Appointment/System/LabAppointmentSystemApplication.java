eef3c2e84be4c73439ebf93147eb271d61992084
Bishan Kanthana <90856608+Bishan00@users.noreply.github.com>
2024-03-13 14:42:11 +0530

    new update
    new update

---
+++ Lab Appointment System/Lab-Appointment-System/src/main/java/com/example/Lab/Appointment/System/LabAppointmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Lab.Appointment.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabAppointmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabAppointmentSystemApplication.class, args);
+	}
+
+}
---
+++ Lab Appointment System/Lab-Appointment-System/src/test/java/com/example/Lab/Appointment/System/LabAppointmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lab.Appointment.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabAppointmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
